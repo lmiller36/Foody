366032a883a77e9e85a0dba5e2467ac4095f285d
Lorne <lmiller36@wisc.edu>
2018-09-23 09:59:19 -0500

    Working touch w/o menu

---
+++ IceCreamBuilderMessagesExtension/ForceTouchGestureRecognizer.swift
@@ -0,0 +1,9 @@
+//
+//  ForceTouchGestureRecognizer.swift
+//  IceCreamBuilderMessagesExtension
+//
+//  Created by Lorne on 9/23/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Foundation
